caadb3ef4cd6608c4ccc128efbf641c2312f1e36
Dima Gaichukov <Dima_russla@mail.ru>
2024-07-09 23:57:34 +0300

    added database

--- app/api/shemas.py
+++ app/api/shemas.py
@@ -1,7 +1,9 @@
+from typing import Annotated
 from pydantic import BaseModel, EmailStr
+from fastapi import Form
 
 class UsersShemas(BaseModel):
-    username: str
-    password: bytes
-    email: EmailStr
+    username: str 
+    password: bytes | str
+    email: EmailStr 
     active: bool = True
--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from fastapi import FastAPI
 import uvicorn
 from app.api.endpoints.auth import auth_rout
 from app.api.endpoints.endpoints import currencies_router
+from app.database.orm import create_tables
 app = FastAPI()
 
 app.include_router(auth_rout)
 app.include_router(currencies_router)
 
 if __name__ == '__main__':
+
+    # create_tables()
+    
     uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True)
